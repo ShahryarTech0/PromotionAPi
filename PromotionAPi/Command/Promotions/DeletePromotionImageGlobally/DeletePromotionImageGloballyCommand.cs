8bac9e6de83e7666ddd96a79784692f5f5f8140e
Shahryar Ali <shahryarbaloch27@gmail.com>
2025-11-28 14:32:13 +0500

    Delete PromotionAPi directory

--- PromotionAPi/Command/Promotions/DeletePromotionImageGlobally/DeletePromotionImageGloballyCommand.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-using Merchants.Application.Responses;
-using System;
-
-namespace Merchants.Application.Commands.Promotions.DeletePromotionImageGlobally
-{
-    public record DeletePromotionImageGloballyCommand(Guid ImageId) : IRequest<Response>;
-}
